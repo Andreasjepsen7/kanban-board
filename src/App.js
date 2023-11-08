683ea2bb8ff1f976f8485153856cb0b3410b6287
Andreas <Andreas@LAPTOP-SPJCEGNK>
2023-11-08 08:30:39 +0100

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const initialColumns = {
+  todo: {
+    id: "todo",
+    title: "To Do",
+    cards: [
+      { id: "task1", content: "Task 1" },
+      { id: "task2", content: "Task 2" },
+      { id: "task3", content: "Task 3" },
+    ],
+  },
+  inProgress: {
+    id: "inProgress",
+    title: "In Progress",
+    cards: [
+      { id: "task4", content: "Task 4" },
+      { id: "task5", content: "Task 5" },
+    ],
+  },
+  done: {
+    id: "done",
+    title: "Done",
+    cards: [{ id: "task6", content: "Task 6" }],
+  },
+};
 
 function App() {
+  const [columns, setColumns] = useState(initialColumns);
+  const [draggedCard, setDraggedCard] = useState(null);
+  const [editingCard, setEditingCard] = useState(null);
+  const [editedContent, setEditedContent] = useState("");
+  const [newCardContent, setNewCardContent] = useState("");
+
+  const handleDragStart = (e, card) => {
+    setDraggedCard(card);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (columnId) => {
+    if (draggedCard) {
+      const updatedColumns = { ...columns };
+      const sourceColumnId = Object.keys(updatedColumns).find(
+        (key) => updatedColumns[key].cards.includes(draggedCard)
+      );
+
+      if (sourceColumnId !== columnId) {
+        // Move the card to a different column
+        updatedColumns[sourceColumnId].cards = updatedColumns[
+          sourceColumnId
+        ].cards.filter((card) => card.id !== draggedCard.id);
+
+        updatedColumns[columnId].cards.push(draggedCard);
+        setColumns(updatedColumns);
+      }
+
+      setDraggedCard(null);
+    }
+  };
+
+  const handleEditCard = (card) => {
+    setEditingCard(card);
+    setEditedContent(card.content);
+  };
+
+  const handleSaveCard = () => {
+    if (editingCard) {
+      const updatedColumns = { ...columns };
+      const cardToUpdate = updatedColumns[editingCard.columnId].cards.find(
+        (card) => card.id === editingCard.id
+      );
+
+      if (cardToUpdate) {
+        cardToUpdate.content = editedContent;
+        setColumns(updatedColumns);
+        setEditingCard(null);
+      }
+    }
+  };
+
+  const handleAddNewCard = (columnId) => {
+    if (newCardContent) {
+      const newCard = { id: `task${Date.now()}`, content: newCardContent };
+      const updatedColumns = { ...columns };
+      updatedColumns[columnId].cards.push(newCard);
+      setColumns(updatedColumns);
+      setNewCardContent("");
+    }
+  };
+
+  const handleDeleteCard = (card) => {
+    const updatedColumns = { ...columns };
+    const column = updatedColumns[card.columnId];
+    column.cards = column.cards.filter((c) => c.id !== card.id);
+    setColumns(updatedColumns);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ display: "flex" }}>
+      {Object.values(columns).map((column) => (
+        <div
+          key={column.id}
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={() => handleDrop(column.id)}
+          style={{
+            background: "#ccc",
+            padding: "16px",
+            width: "300px",
+            margin: "8px",
+            border: "1px solid #999",
+            borderRadius: "10px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h3>{column.title}</h3>
+          {column.cards.map((card) => (
+            <div
+              key={card.id}
+              draggable
+              onDragStart={(e) => handleDragStart(e, card)}
+              style={{
+                userSelect: "none",
+                padding: "12px",
+                margin: "8px",
+                backgroundColor: "#000",
+                color: "#fff",
+                border: "1px solid #333",
+                borderRadius: "6px",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              {editingCard && editingCard.id === card.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={editedContent}
+                    onChange={(e) => setEditedContent(e.target.value)}
+                  />
+                  <button onClick={handleSaveCard}>Save</button>
+                </div>
+              ) : (
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleEditCard({ id: card.id, columnId: column.id })}
+                >
+                  {card.content}
+                </div>
+              )}
+              <button onClick={() => handleDeleteCard(card)}>Delete</button>
+            </div>
+          ))}
+          <div>
+            <input
+              type="text"
+              placeholder="Add a new card"
+              value={newCardContent}
+              onChange={(e) => setNewCardContent(e.target.value)}
+            />
+            <button onClick={() => handleAddNewCard(column.id)}>Add</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
